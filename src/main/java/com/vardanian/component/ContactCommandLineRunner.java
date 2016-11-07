5282bfcfdd435bf3bfb08ab6a061bc1b86ec2ddd
VardanianAkop <AkopVV@gmail.com>
2016-11-07 11:35:56 +0200

    refactoring

--- src/main/java/com/vardanian/component/ContactCommandLineRunner.java
+++ src/main/java/com/vardanian/component/ContactCommandLineRunner.java
@@ -1,17 +1,20 @@
 package com.vardanian.component;
 
+import com.vardanian.entity.Contact;
 import com.vardanian.service.ContactService;
 import com.vardanian.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class ContactCommandLineRunner implements CommandLineRunner {
 
-    ContactService contactService;
+    private ContactService contactService;
 
-    Utils utils = Utils.getInstance();
+    private Utils utils = Utils.getInstance();
 
     @Autowired
     public ContactCommandLineRunner(ContactService contactService) {
@@ -20,6 +23,7 @@ public class ContactCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        this.contactService.save(utils.recordDBContacts());
+        List<Contact> contacts = utils.recordDBContacts();
+        this.contactService.save(contacts);
     }
 }
